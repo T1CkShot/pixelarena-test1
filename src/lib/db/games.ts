1a3eb5596fb113ba856d23ceff8e3f17775a7b6b
T1CkShot <fatta.mmg@gmail.com>
2024-02-08 04:25:59 +0600

    add dummy game data

---
+++ src/lib/db/games.ts
@@ -0,0 +1,23 @@
+export type Game = {
+	title: string;
+	cardImage: string;
+};
+
+export const games: Game[] = [
+	{
+		title: 'PUBG Mobile',
+		cardImage: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1njn.png'
+	},
+	{
+		title: 'Mobile Legends',
+		cardImage: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co7lqm.png'
+	},
+	{
+		title: 'Call of Duty: Mobile',
+		cardImage: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1vqe.png'
+	},
+	{
+		title: 'Clash Royale',
+		cardImage: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1mb7.png'
+	}
+];
