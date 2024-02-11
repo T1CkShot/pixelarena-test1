6874914f0b5bc6e8bf314695ea5d6df0a33eaf67
T1CkShot <fatta.mmg@gmail.com>
2024-02-11 23:35:03 +0600

    add icons to card

---
+++ src/lib/components/CardIconText.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import 'iconify-icon';
+
+	export let textItem: string;
+	export let icon: string;
+</script>
+
+<div class="flex items-center gap-1 text-muted-foreground">
+	<iconify-icon {icon} />
+	<p class="text-md font-semibold">
+		{textItem}
+	</p>
+</div>
